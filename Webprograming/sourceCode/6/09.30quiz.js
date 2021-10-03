b456ff111f441e9e63a1b875943845007d9a62ce
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2021-10-04 00:55:44 +0900

    Update 09.30quiz.js

--- Webprograming/sourceCode/6/09.30quiz.js
+++ Webprograming/sourceCode/6/09.30quiz.js
@@ -1,3 +1,16 @@
+// // for (var i =0; i< a.length; i++){ 
+// //     if (!max) {
+// //         max = a[i];
+// //     }
+// // }
+// // console.log(max);
+
+// // let a = [4, 3, 9, 1, 3, 8];
+// // let max = a[0];
+
+// max = Math.max.apply(null, a);
+// console .log(max);
+
 
 let a = [4, 3, 9, 1, 3, 8];
 let max = a[0];
