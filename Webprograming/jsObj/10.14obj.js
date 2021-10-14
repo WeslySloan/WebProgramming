b82976bd51a3f33321a5dcd68199d66266d40eaa
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2021-10-14 18:09:43 +0900

    Create 10.14obj.js

---
+++ Webprograming/jsObj/10.14obj.js
@@ -0,0 +1,10 @@
+let chulsu = {
+    firstName : "Chulsu",
+    lastName : "Kim",
+};
+let person1 = {
+    name : chulsu,
+    age : 29,
+    nationality: 'Korea',
+};
+console.log(person1.name.lastName);
