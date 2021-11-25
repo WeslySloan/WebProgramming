a06b221c1b2051c3981f56e5cb64994abd38fb99
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2021-11-25 16:07:58 +0900

    Create 11.25.quiz.js

---
+++ Webprograming/11.25.quiz.js
@@ -0,0 +1,21 @@
+function Person(name, age) {
+    this.name = name; this.age = age; }
+
+let people = 
+    [ new Person('B', 31),
+      new Person('AB', 28),
+      new Person('AC', 30),];
+
+    // destructuring constructor
+
+    let [p1, p2, p3] = people
+    let {name,age} = p3
+    console.log(name, age); 
+    
+    let { name:str , age:num } = p2;
+    console.log(str,num);
+
+    let xs = people.map(s => s.name);
+    let ys = people.map( s=> s.age);
+    console.log(xs);
+    console.log(ys);
