1e938cf39892b4606952877e25c4ca0e50f71c4e
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2021-12-13 11:36:05 +0900

    update finalTest

---
+++ 2018975021김현수 기말과제/todoList-basic.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+// Todo List에 추가하는 것 까지
+
+export default function App() {
+  const [todo, setTodo] = useState('')
+  const [todos, setTodos] = useState([])
+
+  const handleInput = e => {
+    // console.log(e.target.value);
+    setTodo(e.target.value);
+  }
+
+  const handleAdd = e => {
+    if (todo === "") return;
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: todos.length + 1, text: todo, completed: false }]
+    })
+    console.log(todos);
+    setTodo("");
+  }
+
+  return (
+    <div className="App">
+      <h3>할일</h3>
+      <ul>
+        {todos.map(todo => <li key={todo.id}> {todo.text} </li>)}
+      </ul>
+      <input onChange={handleInput} value={todo} />
+      <button onClick={handleAdd}>추가</button>
+    </div>
+  );
+}
