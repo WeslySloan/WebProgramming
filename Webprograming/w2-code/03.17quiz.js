49888cab7a4d6ad2b475754996936d4921946e06
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2022-03-18 21:52:25 +0900

    Create 03.17quiz.js

---
+++ Webprograming/w2-code/03.17quiz.js
@@ -0,0 +1,54 @@
+
+
+
+function Circle (radius, name) {
+    this.radius = radius
+    this.name = name
+    this.radius = fuction () { 
+        return 3.14 * this.radius * this.radius;
+    };
+}
+
+
+    var pizza = new Circle(10, "자바피자");
+
+    return consoloe.log(name+"의 면적은"+area);
+
+
+//
+
+2번문제
+
+function getArea() {
+    return 3.14 * this.radius * this.radius;
+}
+
+var pizza = {
+    radius:10;
+    name: "자바피자"
+}
+
+var donut = {
+    radius:2;
+    name: "자바도넛"
+}
+console.log(name + "의 면적은 " + pizza.getArea());
+console.log(name + "의 면적은 " + donut.getArea());
+
+// 
+
+3번문제
+
+
+function Circle(radius, name) {
+    this.radius = radius;
+    this.name = name;
+    this.getArea = function () {
+        return 3.14 * this.radius * this.radius;
+    };
+}
+
+var pizza = new Circle(10, "자바피자");
+var donut = new Circle(2, "자바도넛");
+console.log(pizza.name + "의 면적은 " + pizza.getArea());
+console.log(donut.name + "의 면적은 " + donut.getArea());
