53a6dd889fa474c703cf851958bfe91c788af3b3
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2022-04-04 16:58:40 +0900

    add review ( 04.05 homewokr )

---
+++ Webprograming/review/Ex201.jsx
@@ -0,0 +1,10 @@
+export default function Ex201() {
+  document.title = "첫 타이틀"
+  return (
+    <div>
+      <title>첫 타이틀</title>
+      <div> 페이지에 타이틀을 다는 예제 입니다. 타이틀은 브라우저의 타이틀바에 보여집니다.
+      </div>
+    </div>
+  )
+}
