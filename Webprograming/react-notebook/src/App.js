bc884574eaba1985cd802bfc8e3e4ceccb14e50f
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2022-05-14 22:20:41 +0900

    Update App.js

--- Webprograming/react-notebook/src/App.js
+++ Webprograming/react-notebook/src/App.js
@@ -1,14 +1,24 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+const initialState = { color: "red" }
+
+const reducer = (state, action) => {
+    switch (action.type) {
+      case 'spanStyle':
+        return { color: "green", fontSize: "30px", display: "block", width: "6em", border: "3px dotted magenta", margin: "20px" }
+      default:
+        throw new Error();
+    }
+  }
 
 const App = () => {
-  const [spanStyle, setSpanstyle] = useState({ color: "red" })
-  const newStyle = { color: "green", fontSize: "30px", display: "block", width: "6em", border: "3px dotted magenta", margin: "20px" }
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <div>
       <h3> CSS 스타일 동적 변경</h3>
       <hr />
-      <p style={{ color: "blue" }}>  이것은 <span style={spanStyle}> 문장입니다. </span> </p>
-      <input type="button" value="스타일변경" onClick={() => setSpanstyle(newStyle)} />
+      <p style={{ color: "blue" }}>  이것은 <span style={state}> 문장입니다. </span> </p>
+      <input type="button" value="스타일변경" onClick={() => dispatch({type : 'spanStyle'})} />
     </div>
   );
 };
