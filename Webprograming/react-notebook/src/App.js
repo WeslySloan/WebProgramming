bb70aa7fefd7a58b0834b1fadab7eeb21f49b09d
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2022-05-18 14:41:14 +0900

    sortReact2.jsx
    table.css

--- Webprograming/react-notebook/src/App.js
+++ Webprograming/react-notebook/src/App.js
@@ -1,76 +1,234 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import './table.css';          // .table th   .table td
 
-// 입력 : handleInput, handleAdd
-// 처리 완료 : togggleTodo, handleDoneTodo
-//  toggleTodo에서 todos가 사용되므로, toggleTodo를 Todo 컴포넌트에 넣을 수 없음
-//  toggleTodo 은 top-level에서 정의되어, TodoList -> Todo 로 패스됨
-// 삭제 : handleDelete
 
-export default function TodoComplete() {
-  const [todo, setTodo] = useState('')
-  const [todos, setTodos] = useState([])
+// ---- Data Creation -----------------------------------------
+class Student {
+  constructor(name, age, enrolled, score) {
+    this.name = name; this.age = age; this.enrolled = enrolled; this.score = score;
+  }
+}
 
-  const handleInput = e => {
-    setTodo(e.target.value)
+const data = [
+  new Student('B', 28, false, 70),
+  new Student('E', 18, true, 80),
+  new Student('A', 29, true, 95),
+  new Student('D', 40, false, 67),
+  new Student('C', 30, true, 92),
+];
+
+// -------------------------------------------------------------
+
+export default function SortReact2() {
+  const [studs, setStuds] = useState(data);
+
+  const rows = [];
+  studs.forEach((stud) => rows.push(<BodyRow stud={stud} key={stud.name} />));
+
+  const sortBy = (key) => {
+    const studsCopy = [...studs];
+    const compareBy = (key) => (a, b) => (a[key] < b[key]) ? -1 : ((a[key] > b[key]) ? 1 : 0);
+    studsCopy.sort(compareBy(key));
+    setStuds(studsCopy);
   }
-  const handleAdd = e => {
-    e.preventDefault();
-    if (todo === "") return;
-    setTodos(prevTodos => {
-      return [...prevTodos, { id: todos.length + 1, title: todo, completed: false }]
-    })
-    setTodo('')
-  }
-  const handleDelete = () => {
-    const newTodos = todos.filter(todo => !todo.complete) 
-    setTodos(newTodos)
-  }
-  const toggleTodo = (id) => {
-    const newTodos = [...todos]
-    const done = newTodos.find(todo => todo.id === id) // done is an object. reference type
-    done.complete = !done.complete
-    setTodos(newTodos)
-  }
+
   return (
-    <div>
-      <h3>Todo List</h3>
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <input value={todo} onChange={handleInput} />
-      <button onClick={handleAdd}>Add</button>
-      <button onClick={handleDelete}>Delete</button>
-      <div> Numbers : {todos.filter(todo => !todo.complete).length}</div>
-    </div>
+    <table className="table">
+      <thead className="header">
+        <tr>
+          <th onClick={() => sortBy('name')}>이름</th>
+          <th onClick={() => sortBy('age')}>나이</th>
+          <th onClick={() => sortBy('enrolled')}>등록</th>
+          <th onClick={() => sortBy('score')}>점수</th>
+        </tr>
+      </thead>
+      <tbody className="body">
+        {rows}
+      </tbody>
+    </table>
   );
 }
 
-function TodoList({ todos, toggleTodo }) {
+const BodyRow = ({ stud }) => {
   return (
-    todos.map(todo => {
-      return <Todo
-        key={todo.id} // Child가 여러 개이므로, 각 Todo 에 id가 주어짐
-        todo={todo} toggleTodo={toggleTodo} />
-    })
-  )
-}
-
-function Todo({ todo, toggleTodo }) {
-  const handleDoneClick = () => {
-    toggleTodo(todo.id)
-  }
-  return (
-    <div>
-      <label>
-        <input type="checkbox"
-          checked={todo.complete}
-          onChange={handleDoneClick} />
-        {todo.title}
-      </label>
-    </div>
-  )
+    <tr>
+      <td>{stud.name}</td>
+      <td>{stud.age}</td>
+      <td>{stud.enrolled ? "재학" : "휴학"}</td>
+      <td>{stud.score}</td>
+    </tr>
+  );
 }
 
 
+// import React from 'react';
 
+// // https://ko.reactjs.org/docs/thinking-in-react.html
+
+
+// // ---- Data ------------------------------------------------
+
+// // the creation of objects
+// class Student {
+//   constructor(name, age, enrolled, score) {
+//     this.name = name; this.age = age;
+//     this.enrolled = enrolled; this.score = score;
+//   }
+// }
+
+// let studs = [
+//   new Student('B', 28, false, 70),
+//   new Student('E', 18, true, 80),
+//   new Student('A', 29, true, 95),
+//   new Student('D', 40, false, 67),
+//   new Student('C', 30, true, 92),
+// ];
+
+// // ---------------------------------------------------
+
+// const TableCreate = () =>  <CreateTable studs={studs} />
+
+// const CreateTable = ({ studs }) => {
+//   const headRows = [];
+//   const rows = [];
+//   headRows.push(<HeadRow key={0} />)
+//   studs.forEach((stud) => {
+//     rows.push(<BodyRow stud={stud} key={stud.name} />)
+//   })
+  
+//   return (
+//     <table border="1px">
+//       {headRows}
+//       <tbody>
+//         {rows}
+//       </tbody>
+//     </table>
+//   )
+// }
+
+// const HeadRow = () => {
+//   return (
+//     <thead>
+//       <tr>
+//         <td>이름</td><td>나이</td> <td>등록</td><td>점수</td>
+//       </tr>
+//     </thead>
+//   )
+// }
+
+// const BodyRow = ({ stud }) => {
+//   return (
+//     <tr>
+//       <td>{stud.name}</td>
+//       <td>{stud.age}</td>
+//       <td>{stud.enrolled ? "재학" : "휴학"}</td>
+//       <td>{stud.score}</td>
+//     </tr>
+//   );
+// }
+
+// export default TableCreate;
+// import { useContext } from 'react';
+
+// const UseContextPass = () => {
+//   const themes = {
+//     light: {
+//       foreground: "#000000", // black
+//       background: "#eeeeee"  // white gray
+//     },
+//     dark: {
+//       foreground: "#ffffff",  // white
+//       background: "#222222"   // close to black
+//     }
+//   };
+//   return (
+//     <Toolbar  themes={themes.light} />
+//   )
+// }
+
+// const Toolbar = ({ themes }) => <div> <ThemedButton  themes={themes} /> </div>
+
+// const ThemedButton = ({ themes }) => {
+//   const theme = useContext(ThemeContext);
+//   return (
+//     <button style={{ background: themes.background, color: themes.foreground }}>
+//       I am styled by passing theme!
+//     </button>
+//   );
+// }
+
+// export default UseContextPass;
+
+// import { useState } from 'react';
+
+// // 입력 : handleInput, handleAdd
+// // 처리 완료 : togggleTodo, handleDoneTodo
+// //  toggleTodo에서 todos가 사용되므로, toggleTodo를 Todo 컴포넌트에 넣을 수 없음
+// //  toggleTodo 은 top-level에서 정의되어, TodoList -> Todo 로 패스됨
+// // 삭제 : handleDelete
+
+// export default function TodoComplete() {
+//   const [todo, setTodo] = useState('')
+//   const [todos, setTodos] = useState([])
+
+//   const handleInput = e => {
+//     setTodo(e.target.value)
+//   }
+//   const handleAdd = e => {
+//     e.preventDefault();
+//     if (todo === "") return;
+//     setTodos(prevTodos => {
+//       return [...prevTodos, { id: todos.length + 1, title: todo, completed: false }]
+//     })
+//     setTodo('')
+//   }
+//   const handleDelete = () => {
+//     const newTodos = todos.filter(todo => !todo.complete) 
+//     setTodos(newTodos)
+//   }
+//   const toggleTodo = (id) => {
+//     const newTodos = [...todos]
+//     const done = newTodos.find(todo => todo.id === id) // done is an object. reference type
+//     done.complete = !done.complete
+//     setTodos(newTodos)
+//   }
+//   return (
+//     <div>
+//       <h3>Todo List</h3>
+//       <TodoList todos={todos} toggleTodo={toggleTodo} />
+//       <input value={todo} onChange={handleInput} />
+//       <button onClick={handleAdd}>Add</button>
+//       <button onClick={handleDelete}>Delete</button>
+//       <div> Numbers : {todos.filter(todo => !todo.complete).length}</div>
+//     </div>
+//   );
+// }
+
+// function TodoList({ todos, toggleTodo }) {
+//   return (
+//     todos.map(todo => {
+//       return <Todo
+//         key={todo.id} // Child가 여러 개이므로, 각 Todo 에 id가 주어짐
+//         todo={todo} toggleTodo={toggleTodo} />
+//     })
+//   )
+// }
+
+// function Todo({ todo, toggleTodo }) {
+//   const handleDoneClick = () => {
+//     toggleTodo(todo.id)
+//   }
+//   return (
+//     <div>
+//       <label>
+//         <input type="checkbox"
+//           checked={todo.complete}
+//           onChange={handleDoneClick} />
+//         {todo.title}
+//       </label>
+//     </div>
+//   )
+// }
 
 // import { useReducer } from 'react';
 
